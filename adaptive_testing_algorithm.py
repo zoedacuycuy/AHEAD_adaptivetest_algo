85376331431167a4051b3d16a1749c6ebfc3cf67
Wilyanse <willjose51@gmail.com>
2023-07-26 11:19:49 +0800

    Add python file and TODOs
    Copied TODOs from excel sheet with ChatGPT instructions. Python file to start with, not sure what tech stack to use.

---
+++ adaptive_testing_algorithm.py
@@ -0,0 +1,19 @@
+# TODO: interact with database
+
+# TODO: initialize test with initial set of questions of moderate difficulty
+
+# TODO: present first question to the test taker
+
+# TODO: receive test taker's response and evaluate its correctness
+
+# TODO: calculate test taker's performance metrics based on responses such as number of correct answers and response time
+
+# TODO: select next question from question pool based on adjusted difficulty level
+# TODO: if test taker answers a question correctly within a time frame, increase the difficulty level for the next question
+# TODO: if test taker answers a question incorrectly, decrease difficulty level for the next question
+
+# TODO: Present next question to test taker until test is completed
+
+# TODO: Calculate test taker's final score based on their performance throughout the test
+
+# TODO: Provide immediate feedback on the test taker's performance after each question or at the end of the test
