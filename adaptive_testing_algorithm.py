97517c315492d1f3751467b195acea1c4bfdc985
Wilyanse <willjose51@gmail.com>
2023-07-28 09:46:21 +0800

    Add comments and data analysis
    Added comments for documentation and part that analyzes the feedback based on performance.

--- adaptive_testing_algorithm.py
+++ adaptive_testing_algorithm.py
@@ -25,14 +25,22 @@ testitems_df = pd.read_csv(testitems_path)
 
 # TODO: initialize test with initial set of questions of moderate difficulty
 
-# filters out questions to get moderate difficulty only
+# 1 = easy, 2 = moderate, 3 = hard
+# initialized at moderate
 difficulty_level = 2
 
-num_questions = 10
+
+true_score = 0 # score of student after the test
+expected_score = 0 # depends on probability as calculated by 3PL
+
+starting_proficiency = current_student_proficiency # for stats tracking
+
+num_questions = 10 # number of test items; modify as needed
+# Present next question to test taker until test is completed
 for i in range(num_questions):
     questions_df = testitems_df.query(' diff == {} '.format(difficulty_level))
 
-    # TODO: present question to the test taker
+    # present question to the test taker
     # shuffles the dataframe
     questions_df = questions_df.sample(frac=1)
 
@@ -40,7 +48,7 @@ for i in range(num_questions):
     current_question = questions_df.iloc[0]
     print(current_question)
 
-    # TODO: receive test taker's response and evaluate its correctness
+    # receive test taker's response and evaluate its correctness
     # list of responses that the student can answer
     # IDK is arbitrary for the identification questions
     # temporarily a dictionary; will take in HTML form responses in the future
@@ -49,6 +57,7 @@ for i in range(num_questions):
     # randomizes student answer from list in the dictionary
     # student_response = random.choice(responses_list)
 
+    # uses terminal input for testing
     student_response = input()
 
     # checks if the answer is correct
@@ -56,17 +65,21 @@ for i in range(num_questions):
     if student_response == current_question['answer']:
         correct = 1
     print("correct: " + str(correct))
-    # TODO: calculate test taker's performance metrics based on responses such as number of correct answers and response time
+
+    # calculate test taker's performance metrics based on responses such as number of correct answers and response time
     # uses the 3PL formula to calculate the probability of the student to answer correctly
     probability = current_question['guess'] + ( (1 - current_question['guess']) / (1+math.exp((-1.702 * (current_question['discrim'] / 10) ) * (current_student['proficiency'] - current_question['diff']) )))
     print("probability: " + str(probability))
 
-    # TODO: if test taker answers a question correctly within a time frame, increase the difficulty level for the next question
-    # TODO: if test taker answers a question incorrectly, decrease difficulty level for the next question
+    # if test taker answers a question correctly within a time frame, increase the difficulty level for the next question
+    # if test taker answers a question incorrectly, decrease difficulty level for the next question
     # increase in student's proficiency if the answer is correct and decreases if it is wrong
+    # higher gain/loss if probability is high since this means user does not belong to that difficulty level; true positive
+    # lower gain/loss if probability is low since user performed as expected; false negative
     current_student_proficiency = current_student_proficiency + (( (1 * correct) / 10 * 3) *  probability)
     print("proficiency: " + str(current_student_proficiency))
-    # TODO: select next question from question pool based on adjusted difficulty level
+
+    # select next question from question pool based on adjusted difficulty level
     if current_student_proficiency <= 1:
         difficulty_level = 1
     elif current_student_proficiency <= 2:
@@ -74,9 +87,22 @@ for i in range(num_questions):
     else:
         difficulty_level = 3
 
+# Calculate test taker's final score based on their performance throughout the test
+print("final score: " + str(true_score))
 
-# TODO: Present next question to test taker until test is completed
+# Provide immediate feedback on the test taker's performance after each question or at the end of the test
+# feedback on score
+if expected_score > true_score:
+    print("Underperformed")
+elif expected_score == true_score:
+    print("Performed")
+else:
+    print("Overperformed")
 
-# TODO: Calculate test taker's final score based on their performance throughout the test
-
-# TODO: Provide immediate feedback on the test taker's performance after each question or at the end of the test
+# feedback on proficiency
+if current_student_proficiency > starting_proficiency:
+    print("Improved")
+elif current_student_proficiency == starting_proficiency:
+    print("Maintained")
+elif current_student_proficiency < starting_proficiency:
+    print("Worsened")
