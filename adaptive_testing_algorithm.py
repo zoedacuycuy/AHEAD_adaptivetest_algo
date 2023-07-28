3b67b5cc76f31148a36724a163897f21a59b393a
Wilyanse <willjose51@gmail.com>
2023-07-28 10:01:50 +0800

    Add clamp to proficiency, remove answered question
    Added code to keep track of true and expected score. Added code that removes the question from the question bank to avoid repetition of test items. Clamped proficiency to 0-3 after test was taken.

--- adaptive_testing_algorithm.py
+++ adaptive_testing_algorithm.py
@@ -23,8 +23,7 @@ print("current student prof: " + str(current_student_proficiency))
 # test items table is loaded into the dataframe
 testitems_df = pd.read_csv(testitems_path)
 
-# TODO: initialize test with initial set of questions of moderate difficulty
-
+# initialize test with initial set of questions of moderate difficulty
 # 1 = easy, 2 = moderate, 3 = hard
 # initialized at moderate
 difficulty_level = 2
@@ -38,12 +37,12 @@ starting_proficiency = current_student_proficiency # for stats tracking
 num_questions = 10 # number of test items; modify as needed
 # Present next question to test taker until test is completed
 for i in range(num_questions):
+    # queries pandas data frame to get questions of that difficulty
     questions_df = testitems_df.query(' diff == {} '.format(difficulty_level))
 
     # present question to the test taker
     # shuffles the dataframe
     questions_df = questions_df.sample(frac=1)
-
     # takes the first row of the dataframe as the first question
     current_question = questions_df.iloc[0]
     print(current_question)
@@ -64,12 +63,17 @@ for i in range(num_questions):
     correct = -1
     if student_response == current_question['answer']:
         correct = 1
+        true_score += 1
     print("correct: " + str(correct))
 
     # calculate test taker's performance metrics based on responses such as number of correct answers and response time
     # uses the 3PL formula to calculate the probability of the student to answer correctly
     probability = current_question['guess'] + ( (1 - current_question['guess']) / (1+math.exp((-1.702 * (current_question['discrim'] / 10) ) * (current_student['proficiency'] - current_question['diff']) )))
     print("probability: " + str(probability))
+
+    # if probability is higher than 80%, student should be able to answer
+    if probability >= 0.8:
+        expected_score += 1
 
     # if test taker answers a question correctly within a time frame, increase the difficulty level for the next question
     # if test taker answers a question incorrectly, decrease difficulty level for the next question
@@ -87,6 +91,10 @@ for i in range(num_questions):
     else:
         difficulty_level = 3
 
+    # removes test question from dataframe
+    # avoids repetition of test questions
+    testitems_df.drop(current_question['item_id'], inplace=True)
+
 # Calculate test taker's final score based on their performance throughout the test
 print("final score: " + str(true_score))
 
@@ -98,6 +106,10 @@ elif expected_score == true_score:
     print("Performed")
 else:
     print("Overperformed")
+
+# clamps proficiency to 0-3
+current_student_proficiency = max(0, current_student_proficiency)
+current_student_proficiency = min(3, current_student_proficiency)
 
 # feedback on proficiency
 if current_student_proficiency > starting_proficiency:
