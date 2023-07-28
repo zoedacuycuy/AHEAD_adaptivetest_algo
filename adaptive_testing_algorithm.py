1d1a32685ee3132ee8866ef0907b4c7ca1d7d394
Wilyanse <willjose51@gmail.com>
2023-07-28 08:22:35 +0800

    Take random student; Return probability to answer
    Added code that selects a random student from the dataset in order to have a proficiency variable that serves similar to an MMR system. Probability to answer test item is calculated using 3PL formula.

--- adaptive_testing_algorithm.py
+++ adaptive_testing_algorithm.py
@@ -11,19 +11,26 @@ student_df = pd.read_csv(student_path)
 print(student_df.shape)
 print(student_df.head)
 print(student_df.dtypes)
+
+# student taking the test is assumed to be the first row
+# shuffling the table in order to get random sample of student
+# should be taken from account in UI
+student_df = student_df.sample(frac=1)
+current_student = student_df.iloc[0]
+current_student['proficiency'] = current_student['proficiency'] / 10 * 3
+print("current student prof: " + str(current_student['proficiency']))
+
 testitems_df = pd.read_csv(testitems_path)
-print(testitems_df.shape)
 
 # TODO: initialize test with initial set of questions of moderate difficulty
 
 # filters out questions to get moderate difficulty only
 questions_df = testitems_df.query(' diff == 2 ')
-print(questions_df.head)
 
 # TODO: present first question to the test taker
 # shuffles the datagrame
 questions_df = questions_df.sample(frac=1)
-print(questions_df.head)
+
 # takes the first row of the dataframe as the first question
 firstq = questions_df.iloc[0]
 print(firstq)
@@ -37,7 +44,6 @@ responses_list = ['A', 'B', 'C', 'D', 'TRUE', 'FALSE', 'IDK']
 # randomizes student answer from list in the dictionary
 import random
 student_response = random.choice(responses_list)
-print(student_response)
 
 # checks if the answer is correct
 correct = False
@@ -45,6 +51,10 @@ if student_response == firstq['answer']:
     correct = True
 
 # TODO: calculate test taker's performance metrics based on responses such as number of correct answers and response time
+import math
+probability = firstq['guess'] + ( (1 - firstq['guess']) / (1+math.exp((-1.702 * firstq['discrim']) * (current_student['proficiency'] - firstq['diff']) )))
+
+print("probability: " + str(probability))
 
 # TODO: select next question from question pool based on adjusted difficulty level
 # TODO: if test taker answers a question correctly within a time frame, increase the difficulty level for the next question
